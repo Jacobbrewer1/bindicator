e7e533cdfd588bc38a80023a9aed4e5d14812dc7
Jacob Brewer <Jacobbrewer2000@outlook.com>
2022-04-01 18:08:07 +0100

    Setting up a thread differently to how it was previously

--- bindicator.go
+++ bindicator.go
@@ -21,16 +21,17 @@ func run() {
 	for {
 		go func() {
 			for _, p := range config.JsonConfigVar.RemoteConfig.People {
-				bins.GetBins(p)
-				if p.BinTomorrow() {
-					go func(person *config.PeopleConfig) {
+				go func(person *config.PeopleConfig) {
+					bins.GetBins(person)
+					if person.BinTomorrow() {
 						log.Printf("%v has a bin tomorrow\n", *person.Name)
 						s := person.GetBinsTomorrow()
 						go email.WaitAndSend(s, person)
-					}(p)
-				} else {
-					log.Printf("%v does not have any bins tomorrow\n", *p.Name)
-				}
+
+					} else {
+						log.Printf("%v does not have any bins tomorrow\n", *person.Name)
+					}
+				}(p)
 			}
 		}()
 		t := time.Now().UTC().Add(time.Hour * 24).Format(helper.TimeLayout)
